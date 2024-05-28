b0ecd0df442664075ae22fffb8a0dbb75adfd681
Restyled.io <commits@restyled.io>
2024-05-28 20:22:28 +0000

    Restyled by prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div>Service-Connect, your friend that knows a friend
-    </div>
-  );
+  return <div>Service-Connect, your friend that knows a friend</div>;
 }
 
 export default App;
