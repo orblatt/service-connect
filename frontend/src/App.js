fc2131609dce4236330a75b76e83e47834236fb8
tomeryahpes <tomeryh@mta.ac.il>
2024-05-28 23:30:47 +0300

    add text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
-
-import logo from "./logo.svg";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div>Service-Connect, your friend that knows a friend</div>;
+  return (
+    <div>Service-Connect, your friend that knows a friend that knows a friend
+    </div>
+  );
 }
 
 export default App;
