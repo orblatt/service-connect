a4c1efd3b427f84dc206fe7053250665b362d113
Restyled.io <commits@restyled.io>
2024-05-28 20:23:12 +0000

    Restyled by clang-format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
-  return <div>Service-Connect, your friend that knows a friend</div>;
+  return <div>Service - Connect,
+         your friend that knows a friend < /div>;
 }
 
 export default App;
