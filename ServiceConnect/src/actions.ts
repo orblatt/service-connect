db4475f77b7ca27f6ce17dd0f31e25795f6455dd
Or Golan <orblatt@gmail.com>
2024-06-03 22:22:07 +0300

    added user-jobad relation and update job functionality

--- ServiceConnect/src/actions.ts
+++ ServiceConnect/src/actions.ts
@@ -1,5 +1,6 @@
 import { JobAd } from 'wasp/entities'
-import { CreateJobAd } from 'wasp/server/operations'
+import { HttpError } from 'wasp/server'
+import { CreateJobAd, UpdateJobAd } from 'wasp/server/operations'
 
 type CreateJobAdPayload = Pick<JobAd, 'description'>
 
@@ -7,7 +8,29 @@ export const createJobAd: CreateJobAd<CreateJobAdPayload, JobAd> = async (
   args,
   context
 ) => {
+  if (!context.user) {
+    throw new HttpError(401)
+  }
   return context.entities.JobAd.create({
-    data: { description: args.description },
+    data: { 
+      description: args.description,
+      user: { connect: { id: context.user.id } },
+    },
   })
 }
+
+type UpdateJobAdPayload = Pick<JobAd, 'id' | 'isDone'>
+
+export const updateJobAd: UpdateJobAd<UpdateJobAdPayload,  { count: number } > = async (
+  { id, isDone }, 
+  context
+) => {
+  if (!context.user) {
+    throw new HttpError(401)
+  }
+  return context.entities.JobAd.updateMany({
+    where: { id, user: { id: context.user.id } },
+    data: { isDone },
+  })
+}
+
--- ServiceConnect/src/queries.ts
+++ ServiceConnect/src/queries.ts
@@ -1,8 +1,13 @@
 import { JobAd } from 'wasp/entities'
+import { HttpError } from 'wasp/server'
 import { type GetJobAds } from 'wasp/server/operations'
 
 export const getJobAds: GetJobAds<void, JobAd[]> = async (args, context) => {
+  if (!context.user) {
+    throw new HttpError(401)
+  }
   return context.entities.JobAd.findMany({
+    where: { user: { id: context.user.id } },
     orderBy: { id: 'asc' },
   })
 }
