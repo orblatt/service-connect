ec1efe58ba0ec71ed83e01131386420f21f7813f
Or Golan <orblatt@gmail.com>
2024-06-29 12:38:54 +0300

    add routes to navbar

---
+++ ServiceConnect/src/client/app/MainLayout.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React from 'react';
+import {
+  Box,
+  useColorModeValue,
+  Drawer,
+  DrawerContent,
+  useDisclosure
+} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import MobileNav from '../components/sidebar/MobileNav'
+import SidebarContent from '../components/sidebar/SidebarContent'
+
+const MainLayout = () => {
+  return (
+    <ChakraProvider>
+      <SidebarWithHeader />
+    </ChakraProvider>
+  )
+}
+
+const SidebarWithHeader = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+      </Box>
+    </Box>
+    )
+}
+
+export default MainLayout;
--- ServiceConnect/src/config.ts
+++ ServiceConnect/src/config.ts
@@ -3,3 +3,9 @@ export const defaultMaxPrice: string = '99999';
 export const defaultSearchProfile = {
     interval: 'minutely'
 };
+export const routes = {
+    home: '/',
+    createJobAd: '/create-job-ad',
+    searchJobAds: '/search-job-ads',
+    test: '/test',
+};
