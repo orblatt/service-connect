32b67d6cb1ed6c6fe359113bc60259d68cc77cbb
Or Golan <orblatt@gmail.com>
2024-07-03 15:41:10 +0300

    create homepage

--- ServiceConnect/src/client/app/MainPage.tsx
+++ ServiceConnect/src/client/app/MainPage.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { AuthUser } from 'wasp/auth'
-import { Logout } from '../components/Logout';
-import MainLayout from './Layout'
-
+import { Card, CardBody, CardHeader, Heading, Text } from "@chakra-ui/react"
+import MainLayout from "./MainLayout"
+import { AuthUser } from "wasp/auth"
+import { useUserDetails } from '../../utils'
 
 export const MainPage = ({ user }: { user: AuthUser }) => {
-  return (
-    <MainLayout>
-    <div className="container">
-      <main>
-        <div>
-          Hello world!
-        </div>
-        <Logout />
-      </main>
-    </div>
-    </MainLayout>
-  );
-};
+    const username = user.id ? useUserDetails(user.id, 'Owner') : '';
+    return (
+        <MainLayout user={user}>
+            <Card>
+                <CardHeader>
+                    <Heading>Welcome {username}</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Text lineHeight='taller'>
+                        Welcome to Service Connect! This is a platform that connects service providers with customers.<br/>
+                        As a service provider, you can create job ads and get hired by customers.<br/>
+                        As a customer, you can browse job ads and hire service providers.<br/>
+                        <b>Are you ready?</b>
+                    </Text>
+                    <Text>
+
+                    </Text>
+                </CardBody>
+            </Card>
+        </MainLayout>
+    )
+}
